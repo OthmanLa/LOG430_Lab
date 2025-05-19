dcff7263bf5c9e1640637abd9ea396cecfdf5145
Othman <laazirio2@gmail.com>
2025-05-19 20:26:18 +0000

    final test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,7 @@
 from app.main import hello
 
 def test_hello_returns_string():
-    """La fonction doit renvoyer une chaîne."""
     assert isinstance(hello(), str)
 
 def test_hello_value():
-    """Le contenu exact doit être 'Hello, World!'."""
     assert hello() == "Hello, World!"
