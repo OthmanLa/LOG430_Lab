495cea0b129e00f8ce2e743d23621f0542defad9
Othman <laazirio2@gmail.com>
2025-06-10 21:28:57 +0000

    test

--- tests/confest.py
+++ tests/confest.py
@@ -2,10 +2,7 @@
 import pytest
 from app.db.init_db import init_db
 
-# Automatically initialize the database schema before tests
 @pytest.fixture(scope="session", autouse=True)
 def setup_db():
-    # This will create all tables as per your models
     init_db()
     yield
-    # Optionally, teardown logic here (e.g., drop tables)
