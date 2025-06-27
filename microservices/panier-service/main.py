13223166546427a464db4f5bb4fc01dfed1c3df7
Othman <laazirio2@gmail.com>
2025-06-27 01:54:09 +0000

    lab5 maj

--- microservices/panier-service/main.py
+++ microservices/panier-service/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.routes import paniers
 from prometheus_fastapi_instrumentator import Instrumentator
 
-app = FastAPI()
+app = FastAPI(title="Paniers Service")
 app.include_router(paniers.router, prefix="/api/v1")
 
 Instrumentator().instrument(app).expose(app)
